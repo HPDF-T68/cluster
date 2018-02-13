53e179f5234bad3918dac3393e1be448e0c95260
nehat <nehatiwari.csk@gmail.com>
2018-02-13 16:26:28 +0530

    testing new endpoint

--- microservices/api/src/hasuraExamples.js
+++ microservices/api/src/hasuraExamples.js
@@ -2,10 +2,33 @@ var express = require('express');
 var router = express.Router();
 var config = require('./config');
 var request = require('request');
+var fetchAction =  require('fetch');
 
 router.route("/").get(function (req, res) {
   res.send("Hello world from app")
 })
+
+router.route("/get_bill").get(function(req, res){
+
+
+  var url = "https://filestore.bathtub62.hasura-app.io/v1/file/11f3e127-f34f-44af-b809-7e49e45089f9";
+
+  var requestOptions = {
+      "method": "GET",
+      "headers": {}
+  };
+
+  fetchAction(url, requestOptions)
+  .then(function(response) {
+  	return response.blob();
+  })
+  .then(function(blob) {
+  	console.log(result);
+  })
+  .catch(function(error) {
+  	console.log('Request Failed:' + error);
+  });
+});
 
 router.route("/get_articles").get(function (req, res) {
   //Fetch all rows from table - articles
